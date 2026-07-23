13418a160461e799cade3fcc2eb31da230a425a6
danila <idanila331@gmail.com>
2026-07-23 14:17:31 +0300

    feat: add spinner

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ContactItem from './ContactItem/ContactItem';
 import api from '../../api/contact-service';
@@ -8,15 +8,24 @@ import {
   selectContact,
 } from '../../store/actions/contactActions';
 import styles from './ContactList.module.css';
+import { ClipLoader } from 'react-spinners';
 
 function ContactList() {
   const contacts = useSelector((state) => state.contacts.contacts);
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    api.get('/').then(({ data }) => {
-      dispatch(fetchContactsSuccess(data || []));
-    });
+    setIsLoading(true);
+
+    const fetchPromise = api.get('/');
+    const delayPromise = new Promise((resolve) => setTimeout(resolve, 500));
+
+    Promise.all([fetchPromise, delayPromise])
+      .then(([{ data }]) => {
+        dispatch(fetchContactsSuccess(data || []));
+      })
+      .finally(() => setIsLoading(false));
   }, [dispatch]);
 
   const deleteContact = (id) => {
@@ -32,16 +41,20 @@ function ContactList() {
   return (
     <div className={styles.contactMainList}>
       <h2>Contact List</h2>
-      <ul className={styles.contactList}>
-        {contacts.map((contact) => (
-          <ContactItem
-            key={contact.id}
-            contact={contact}
-            deleteContact={deleteContact}
-            selectContact={handleSelectContact}
-          />
-        ))}
-      </ul>
+      {isLoading ? (
+        <ClipLoader />
+      ) : (
+        <ul className={styles.contactList}>
+          {contacts.map((contact) => (
+            <ContactItem
+              key={contact.id}
+              contact={contact}
+              deleteContact={deleteContact}
+              selectContact={handleSelectContact}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
