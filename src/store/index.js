79a8e43d22ca9fa639b61bac54da008021cf504f
danila <idanila331@gmail.com>
2026-07-28 14:22:46 +0300

    start redux-toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,5 @@
-import { applyMiddleware, createStore } from 'redux';
-import { logger } from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit';
 
-import rootReducer from '../reducers';
-
-const store = createStore(rootReducer, applyMiddleware(logger));
-
-export default store;
+export default configureStore({
+  reducer: () => {},
+});
--- src/store/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import contacts from './contactsReducer';
-
-export default combineReducers({ contacts });
